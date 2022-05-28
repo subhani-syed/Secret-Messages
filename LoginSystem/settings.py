cf2c716fd3b01a407b704b358fd06e1793a855b0
Subhani-syed <subhani6352@gmail.com>
2022-05-28 18:11:41 +0530

    Added Template and Static Files Path

--- LoginSystem/settings.py
+++ LoginSystem/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'LoginSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added Manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
