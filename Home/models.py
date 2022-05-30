72afd456e5ef41a8ea7faa518c35df217502a4fe
Subhani-syed <subhani6352@gmail.com>
2022-05-30 13:14:39 +0530

    Created Message Model

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    body = models.CharField(max_length=100)
+    time = models.DateTimeField(default=None, null=True)
+    uid = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.body
