d842a2c053dcbfbaf76c822ddb602e69601fe551
Subhani-syed <subhani6352@gmail.com>
2022-05-30 16:57:57 +0530

    Added Messages

--- LoginSystem/settings.py
+++ LoginSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 LOGIN_URL = '/login'
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR : 'danger'
+}
