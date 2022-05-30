fa73de53fa009c60787831a60805c4fb87d6e276
Subhani-syed <subhani6352@gmail.com>
2022-05-30 17:17:15 +0530

    Removed django-heroku

--- LoginSystem/settings.py
+++ LoginSystem/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
