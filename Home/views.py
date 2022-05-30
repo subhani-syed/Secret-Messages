fcb134c1e9a5bea0358e2e99dad18332f52006c6
Subhani-syed <subhani6352@gmail.com>
2022-05-30 16:56:46 +0530

    Added Functionality to Endpoints

--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from Home.models import Message
+import datetime
+import hashlib
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,7 +14,9 @@ def home(request):
     cur_user = request.user
     context={
         "user_name":cur_user.username,
-        "user_id":cur_user.id
+        "user_id":cur_user.id,
+        'msgs':reversed(Message.objects.filter(uid = intToHash(cur_user.id))),
+        'hvalue':intToHash(cur_user.id),
     }
     return render(request,'home.html',context)
 
@@ -23,12 +28,17 @@ def login_user(request):
         user = authenticate(request,username=name,password=password)
         if user is not None:
             login(request,user)
+            messages.success(request,"Successfully Logged In")
             return redirect('/')
         else:
-            return HttpResponse("Invalid Login")
+            messages.error(request,"Invalid User")
+            return redirect('/')
     
     # GET
-    return render(request,'login.html')
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        return render(request,'login.html')
 
 def logout_user(request):
     logout(request)
@@ -43,7 +53,45 @@ def register(request):
         fname = request.POST['fname']
         user = User.objects.create_user(username=name,password=password,last_name=lname,first_name=fname)
         user.save()
-        return HttpResponse(f"User created with username-> {name} and password-> {password}")
+        messages.success(request,"User Created Successfully")
+        return redirect("/")
     
     # GET
     return render(request,'register.html')
+
+
+def sendMessage(request,u_id):
+    
+    # To Find The Username from Link
+    obj = User.objects.all()
+    out=""
+    for x in obj:
+        if intToHash(x.id) == u_id:
+            out = x.username
+
+    # Check if the slug is valid or not
+    if(out!=""): #Valid Slug
+        if out == request.user.username: # Logged In User has same Link
+            return redirect("/")
+        else: #Different User
+
+            # POST
+            if request.method == "POST":
+                body = request.POST['body']
+                time = datetime.datetime.now()+datetime.timedelta(hours=5.5)
+                # Incoming url has hash value
+                msg = Message(body=body,time=time,uid=u_id)
+                msg.save()
+                messages.success(request,"Message Sent Successfully")
+                return render(request,'sent.html')
+
+            # GET
+            return render(request,'message.html',{'uname':out,})
+    else: #Invalid Slug
+        messages.error(request,"Invalid link")
+        return redirect("/")
+    
+# Converts Int to Hash Value
+def intToHash(inp):
+    str_inp = f"{inp}"
+    return hashlib.md5(str_inp.encode()).hexdigest()
